fa1003823ca0f29a9cca0b444c376beb708fe2c0
smith <clayton.smith33@gmail.com>
2026-03-09 23:31:38 -0700

    Define minimal Block1 bridge C ABI

---
+++ weather_protocol_rust/target/release/build/compiler_builtins-e085df844229ae40/build_script_build-e085df844229ae40.d
@@ -0,0 +1,6 @@
+/Users/smith/Documents/Personal/Projects/Project Local Weather/projectLocalWeather/weather_protocol_rust/target/release/build/compiler_builtins-e085df844229ae40/build_script_build-e085df844229ae40.d: /Users/smith/.rustup/toolchains/esp/lib/rustlib/src/rust/library/compiler-builtins/compiler-builtins/build.rs /Users/smith/.rustup/toolchains/esp/lib/rustlib/src/rust/library/compiler-builtins/compiler-builtins/configure.rs
+
+/Users/smith/Documents/Personal/Projects/Project Local Weather/projectLocalWeather/weather_protocol_rust/target/release/build/compiler_builtins-e085df844229ae40/build_script_build-e085df844229ae40: /Users/smith/.rustup/toolchains/esp/lib/rustlib/src/rust/library/compiler-builtins/compiler-builtins/build.rs /Users/smith/.rustup/toolchains/esp/lib/rustlib/src/rust/library/compiler-builtins/compiler-builtins/configure.rs
+
+/Users/smith/.rustup/toolchains/esp/lib/rustlib/src/rust/library/compiler-builtins/compiler-builtins/build.rs:
+/Users/smith/.rustup/toolchains/esp/lib/rustlib/src/rust/library/compiler-builtins/compiler-builtins/configure.rs:
---
+++ weather_protocol_rust/target/xtensa-esp32s3-none-elf/release/deps/weather_protocol_rust-58fa6c4bfa159846.d
@@ -0,0 +1,18 @@
+/Users/smith/Documents/Personal/Projects/Project Local Weather/projectLocalWeather/weather_protocol_rust/target/xtensa-esp32s3-none-elf/release/deps/weather_protocol_rust-58fa6c4bfa159846.d: src/lib.rs src/assembler.rs src/ble_adapter.rs src/bridge.rs src/core.rs src/device_state.rs src/display.rs src/driver.rs src/ffi.rs src/ingress.rs src/interpolation.rs src/persistence.rs
+
+/Users/smith/Documents/Personal/Projects/Project Local Weather/projectLocalWeather/weather_protocol_rust/target/xtensa-esp32s3-none-elf/release/deps/libweather_protocol_rust-58fa6c4bfa159846.rlib: src/lib.rs src/assembler.rs src/ble_adapter.rs src/bridge.rs src/core.rs src/device_state.rs src/display.rs src/driver.rs src/ffi.rs src/ingress.rs src/interpolation.rs src/persistence.rs
+
+/Users/smith/Documents/Personal/Projects/Project Local Weather/projectLocalWeather/weather_protocol_rust/target/xtensa-esp32s3-none-elf/release/deps/libweather_protocol_rust-58fa6c4bfa159846.a: src/lib.rs src/assembler.rs src/ble_adapter.rs src/bridge.rs src/core.rs src/device_state.rs src/display.rs src/driver.rs src/ffi.rs src/ingress.rs src/interpolation.rs src/persistence.rs
+
+src/lib.rs:
+src/assembler.rs:
+src/ble_adapter.rs:
+src/bridge.rs:
+src/core.rs:
+src/device_state.rs:
+src/display.rs:
+src/driver.rs:
+src/ffi.rs:
+src/ingress.rs:
+src/interpolation.rs:
+src/persistence.rs:
